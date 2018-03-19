a5c59f8ad09546bb7e69da4986f5e30092a8b52a
happut <happut@163.com>
2018-03-19 23:17:49 +0800

    test

--- vue-playlist/src/App.vue
+++ vue-playlist/src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <h1>{{ title }}</h1>
+    <users/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+
+import Users from './components/Users'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components:{
+    Users
+  },
+  data() {
+    return {
+      title: "这是我第一个脚手架"
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ vue-playlist/src/components/Users.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="users">
+   <ul>
+       <li v-for="user in users">
+           {{ user }}
+        </li>
+   </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "users",
+  data() {
+    return {
+      users: ["wangfei", "test", "taotao"]
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
