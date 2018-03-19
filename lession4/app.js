3ea3e761c4bf29330e8785a4cd4c31578a796590
happut <happut@163.com>
2018-03-19 15:05:11 +0800

    study

--- lession4/app.js
+++ lession4/app.js
@@ -2,28 +2,10 @@
 new Vue({
     el: "#vue-app",
     data: {
-        age: 30,
-        x: 0,
-        y: 0
+
     },
     methods: {
-        add: function (inc) {
-            this.age += inc;
-        },
-        subtract: function (dec) {
-            this.age -= dec;
-        },
-        updateXY: function (event) {
-            console.log(event)
-            this.x = event.offsetX;
-            this.y = event.offsetY;
-        },
-        stopMoving: function (event) {
-            event.stopPropagation();
-        },
-        alert: function () {
-            alert("hello world");
-        }
+
     }
 });
 
